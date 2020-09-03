0592e610f9940467960825be72d9fd865d1d0572
bjoernh2000 <bjoernh@bu.edu>
2020-09-03 14:10:19 +0200

    added alert

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Button,
+  Alert,
+  Platform,
+  StatusBar
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity>
+        <Text style={styles.title}>Contact Tracing</Text>
+      </TouchableOpacity>
+      <View style={styles.loginView}>
+        <Button
+          title="Button"
+          onPress={() =>
+            Alert.alert("Login", "You tried logging in", [
+              {
+                text: "Ok",
+                onPress: () => console.log("Ok")
+              }
+            ])
+          }
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
+  title: {
+    color: "black",
+    fontSize: 50
+  },
+  loginView: {
+    flex: 1,
+    justifyContent: "center"
+  }
 });
